dc7eb1c1608a0254b51142290830ab28f73cc5d0
BuildTools <wolfmurdurer23@gmail.com>
2021-12-18 14:37:48 -0600

    Initial Commit

---
+++ mountfs.py
@@ -0,0 +1,19 @@
+import ctypes
+import ctypes.util
+import socket
+import subprocess
+
+libc = ctypes.CDLL(ctypes.util.find_library('c'), use_errno=True)
+libc.mount.argtypes = (ctypes.c_char_p, ctypes.c_char_p, ctypes.c_char_p, ctypes.c_ulong, ctypes.c_char_p)
+
+def mount(path_to_smb, path_to_local_dir):
+    retcode = subprocess.call(["/usr/bin/mount", "-t", "cifs", "-o", "uid=1000,gid=1000,credentials=/home/monica/.smbcredentials", path_to_smb, path_to_local_dir])
+
+localIP = socket.gethostbyname(socket.gethostname())
+
+if("10.0.0." in localIP):
+    mount('//10.0.0.6/Plex', '/mnt/Plex')
+    mount('//10.0.0.6/monica', '/mnt/home')
+elif("192.168.68." in localIP):
+    mount('//192.168.68.11/Games', '/mnt/Games')
+    mount('//192.168.68.11/monica', '/mnt/home')
